292c6b6737458936224c80cd418bbe0f74427975
noahliechti <noah.liechti@gmail.com>
2022-01-25 23:36:08 +0100

    enable fetching hardcoded tweet

--- functions/image.js
+++ functions/image.js
@@ -1,40 +1,99 @@
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
 const chromium = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer-core");
 
+const TWEET_WIDTH = 1000;
+const TWEET_PADDING = 25;
+const TWEET_HIDE_THREAD = true;
+const TWEET_HIDE_CARD = false;
+
 exports.handler = async function (event, context) {
+  //url theme language
+  const tweetURL =
+    "https://twitter.com/Rainmaker1973/status/1478285768493834240";
+  const theme = "dark";
+  const language = "en";
+
   console.log("event", event.body);
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath:
-      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
-    headless: true,
+
+  const screenshot = await createScreenshot({
+    width: TWEET_WIDTH,
+    theme,
+    padding: TWEET_PADDING,
+    tweetId: getTweetId(tweetURL),
+    hideCard: TWEET_HIDE_CARD,
+    hideThread: TWEET_HIDE_THREAD,
+    language,
   });
-
-  const page = await browser.newPage();
-
-  await page.goto("https://spacejelly.dev/");
-
-  const title = await page.title();
-  const description = await page.$eval(
-    'meta[name="description"]',
-    (element) => element.content
-  );
-
-  await browser.close();
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      page: {
-        title,
-        description,
-      },
+      image: screenshot,
     }),
   };
 };
+
+const createScreenshot = async (props) => {
+  try {
+    const { language, width, theme, padding, hideCard, hideThread, tweetId } =
+      props;
+
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath:
+        process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+      headless: true,
+      // args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    const page = await browser.newPage();
+    await page.goto(
+      `https://platform.twitter.com/embed/index.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=${hideCard}&hideThread=${hideThread}&id=${tweetId}&language=${language}&theme=${theme}&widgetsVersion=ed20a2b%3A1601588405575`,
+      { waitUntil: "networkidle0" }
+    );
+
+    const embedDefaultWidth = 550;
+    const percent = width / embedDefaultWidth;
+    const pageWidth = embedDefaultWidth * percent;
+    const pageHeight = 100;
+    await page.setViewport({ width: pageWidth, height: pageHeight });
+
+    await page.evaluate(
+      (props) => {
+        const { theme, padding, percent } = props;
+
+        const style = document.createElement("style");
+        style.innerHTML =
+          "* { font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol' !important; }";
+        document.getElementsByTagName("head")[0].appendChild(style);
+
+        const body = document.querySelector("body");
+        body.style.padding = `${padding}px`;
+        body.style.backgroundColor = theme === "dark" ? "#000" : "#fff";
+        body.style.zoom = `${100 * percent}%`;
+        const articleWrapper = document.querySelector("#app > div");
+        articleWrapper.style.border = "none";
+      },
+      { theme, padding, percent }
+    );
+
+    const imageBuffer = await page.screenshot({
+      type: "png",
+      fullPage: true,
+      encoding: "base64",
+    });
+
+    await browser.close();
+
+    return imageBuffer;
+  } catch (err) {
+    console.log("Error when creating the image of the tweet: ", err);
+  }
+};
+
+function getTweetId(tweetURL) {
+  const splitTweetURL = tweetURL.split("/");
+  const lastItem = splitTweetURL[splitTweetURL.length - 1];
+  const splitLastItem = lastItem.split("?");
+  return splitLastItem[0];
+}
